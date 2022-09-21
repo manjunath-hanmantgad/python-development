9c2b4468a6efd8803de081210173eeac6ec8836e
Manjunath <hanamanthgad123@gmail.com>
2022-09-21 20:00:59 +0530

    functions project

--- py_codes/functions.py
+++ py_codes/functions.py
@@ -33,3 +33,61 @@ def f2():
 print(f1())
 print(f2())
 print(a) # a here is global variable
+
+
+#########################
+
+print(1,2,3,4,5)
+numbers = [1,2,3,4,5]
+print(numbers)
+
+print(*numbers) # unpacking 
+
+def add (x,y):
+    return x + y
+
+print(add(10,1))
+
+
+def add(*numbers):
+    total =0
+    for n in numbers:
+        total = total + n
+    return(total)
+
+print(add(1,2,3,4,5))
+
+
+def about(name,age,likes):
+    sentence = "Meet {} , they are {},and they like {}".format(name,age,likes)
+    return sentence
+
+dict_1 = {'name':'N1','age':22, 'likes':'apples'}
+print(about(**dict_1))
+
+
+
+
+
+
+def foo(**kwargs):
+    # produce a dict called kwargs 
+    for key,value in kwargs.items():
+        print("{},{}".format(key,value))
+
+foo(animal="cow",likes="me")
+print(foo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
