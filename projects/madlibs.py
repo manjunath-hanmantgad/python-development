7f358def4c683a61a4e757329fc6eadf73585e7b
Manjunath <hanamanthgad123@gmail.com>
2022-09-22 20:10:13 +0530

    pyproject for strings

---
+++ projects/madlibs.py
@@ -0,0 +1,10 @@
+# string concatenation project
+
+# adding an f string
+adjective = input("Adjective")
+verb1 = input("verb")
+madlib = f"Im about to do the {adjective}! , I dont know if it will \
+be {verb1} to achieve this."
+
+print(madlib)
+
