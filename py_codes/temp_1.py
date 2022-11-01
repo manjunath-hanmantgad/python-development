cb5428883da447ef244c85ff25fc063c3c8d1d54
Manjunath <hanamanthgad123@gmail.com>
2022-11-01 17:02:14 +0530

    if

---
+++ py_codes/temp_1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: Manjunath Hanmantgad
+"""
+#furry = True
+furry = False
+#large = True
+large = False
+
+if furry:
+    if large:
+        print("its yeti")
+    else:
+        print("its a cat")
+
+else:
+    if large:
+        print("its a whhell")
+    else:
+        print("WTF")
+    
+
+
+#some_list = []
+some_list = [1,2]
+if some_list:
+    print("present")
+else:
+    print("nothing")
+
+
+# check whether letter is vovel ?
+
+vowels = 'aeiou'
+letter = 'o'
+
+if letter in vowels:
+    print(letter, 'is a vowel')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
