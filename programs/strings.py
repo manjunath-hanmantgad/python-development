0c165e561524e52f1ea3036eb6b98ab60103b2c2
Manjunath <hanamanthgad123@gmail.com>
2023-02-01 17:48:59 +0530

    strings

---
+++ programs/strings.py
@@ -0,0 +1,18 @@
+# adding whitespace
+print("\tPython")
+
+print("Languages:\nPython\nJava\nJS")
+
+# Stripping Whitespace
+
+# rstrip() and lstrip()
+
+fav_lang = 'java '
+print(fav_lang)
+print(fav_lang.rstrip())
+
+# remove prefix
+
+prefix = 'https://dnd.com'
+print(prefix.removeprefix('https:'))
+print(prefix.removeprefix('https://'))
