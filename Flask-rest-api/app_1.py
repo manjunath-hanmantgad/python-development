1444922633507dfb120884e6c6e7ae36c705e7e2
Manjunath <hanamanthgad123@gmail.com>
2023-02-23 16:32:30 +0530

    dealing with dynamic entries from client

--- Flask-rest-api/app_1.py
+++ Flask-rest-api/app_1.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+app_1 = Flask(__name__)
 
 # where we will store our data 
 
@@ -21,19 +21,19 @@ stores = [
 # information want to retrieve is using GET
 
 #@app.get("/store") #http://127.0.0.1:5000/store
-@app.route('/store', methods=["GET"]) # .get was not working somehow !!
+@app_1.route('/store', methods=["GET"]) # .get was not working somehow !!
 
 def get_scores():
     return {"stores":stores}
 
-@app.route('/store', methods = ["POST"])
+@app_1.route('/store', methods = ["POST"])
 def create_store():
     request_data = request.get_json() # getting the data which client is posting in postman
     new_store = {"name": request_data["name"], "items":[]} # creating a new store 
     stores.append(new_store)
     return new_store, 201 # 201 is status code
 
-@app.route("/store/<string:name>/item", methods=["POST"])
+@app_1.route("/store/<string:name>/item", methods=["POST"])
 def create_item(name): # name part of string:name is passed into the parameter
     request_data = request.get_json() # grab the data entered from postman
     # match the store name 
