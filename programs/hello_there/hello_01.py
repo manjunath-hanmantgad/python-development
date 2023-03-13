fcb64fb53ee0fe229067e105bf8fc29f16b29854
Manjunath_Hanmantgad <hanamanthgad123@gmail.com>
2023-03-13 23:19:51 +0530

    Using main() function

--- programs/hello_there/hello_01.py
+++ programs/hello_there/hello_01.py
@@ -2,7 +2,13 @@
 #print("Hello , World!")
 
 import argparse
-parser = argparse.ArgumentParser(description="Say Hello")
-parser.add_argument('name', help="Name to greet")
-args = parser.parse_args()
-print('Hello,' + args.name + '!')
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument('name', help="Name to greet")
+    args = parser.parse_args()
+    print('Hello,' + args.name + '!')
+
+if __name__=='__main__':
+    main()
