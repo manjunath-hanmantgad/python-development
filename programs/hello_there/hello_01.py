f0e291d6ea44c2f56ec30804cf6e6c7e2b2e4e23
Manjunath_Hanmantgad <hanamanthgad123@gmail.com>
2023-03-13 23:28:23 +0530

    Modularizing functions for arg parser

--- programs/hello_there/hello_01.py
+++ programs/hello_there/hello_01.py
@@ -3,11 +3,27 @@
 
 import argparse
 
-def main():
-
+def get_args():
+    ''' get_args() function is dedicated to getting the arguments'''
     parser = argparse.ArgumentParser(description="Say Hello")
     parser.add_argument('name', help="Name to greet")
-    args = parser.parse_args()
+    # args = parser.parse_args()
+    ''' 
+    call return to send the results of 
+    parsing the arguments back to the main() function.
+    '''
+    return parser.parse_args()
+
+def main():
+
+    # parser = argparse.ArgumentParser(description="Say Hello")
+    # parser.add_argument('name', help="Name to greet")
+    # args = parser.parse_args()
+    # call the get_args function here
+    args = get_args()
+    '''
+    Call the get_args() function to get parsed arguments.
+    '''
     print('Hello,' + args.name + '!')
 
 if __name__=='__main__':
