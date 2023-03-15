b07a4adb45adb99d7dd781f3c7703a59beca337d
Manjunath <hanamanthgad123@gmail.com>
2023-03-15 18:41:40 +0530

    oops

---
+++ oops/oops_02.py
@@ -0,0 +1,59 @@
+
+# we have a company
+# represent the employyes via this class
+# each employee wll have different attributes 
+
+class Employee:
+    # creating init method -- based on A
+    def __init__(self,first,last,email):
+        '''giving it here so that we dont need define indivudally'''
+        self.first=first
+        self.last=last
+        self.email=first + '.' + last + '@gmail.com'
+        
+
+# each employee will be attibute of this class employee
+
+''' 
+emp_1 = Employee()
+emp_2 = Employee()
+'''
+
+# now creating based on class
+
+emp_1 = Employee('M','H',1)
+emp_2= Employee('Test','User',1)
+
+# there was error that I need to give 3 arguments or inputs
+# when calling the emp_1.email
+# but when I gave something else in 3rd position then 
+# it did not throw the error.
+
+
+
+
+
+# print(emp_1) # different addresses
+# print(emp_2)
+
+# # employees have fist and last names
+
+# emp_1.first = "M"
+# emp_1.last = "H"
+# emp_1.email = "MH@gmail.com"
+# emp_1.pay = 100
+
+
+# emp_2.first = "Test"
+# emp_2.last = "User"
+# emp_2.email = "TestUser@gmail.com"
+# emp_2.pay = 200
+
+print(emp_1.email)
+print(emp_2.email)
+
+# so rather than manually doing this 
+# need to set it up in the initail stage itself.
+
+# using __init__ method -- A
+
