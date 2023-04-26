6c478ab3dcc4fb611b59eddad20fe1286ecf9ae5
Manjunath <hanamanthgad123@gmail.com>
2023-04-26 18:43:48 +0530

    lists

---
+++ programs/list.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 18:37:06 2023
+
+@author: Manjunath
+"""
+
+bicycles = ['trek','cannon','redline','special']
+
+#Accessing Elements in a List
+bicycles[0]
+bicycles[0].title()
+bicycles[-1]
+
+#Using Individual Values from a List
