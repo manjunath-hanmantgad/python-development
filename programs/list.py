40ec30a3e885d170f973ae38521a11a6b71abacc
Manjunath <hanamanthgad123@gmail.com>
2023-04-26 18:49:34 +0530

    lists

--- programs/list.py
+++ programs/list.py
@@ -13,3 +13,22 @@ bicycles[0].title()
 bicycles[-1]
 
 #Using Individual Values from a List
+# use f strings for this 
+
+message = f"My first bicycle is {bicycles[0].title()}"
+print(message)
+
+
+names = ['a','b','c','d']
+names[0]
+names[1]
+names[2]
+names[3]
+
+message = f"Hello there {names[0].title()}" #names[1,2,3]
+
+bikes = ['honda','crv']
+message = f"I would like to ride {bikes[0]}"
+print(message)
+message_1 = f"I would like to drive {bikes[1]}"
+print(message_1)
