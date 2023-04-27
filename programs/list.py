cf3b6d92980cc5c31c7658a8d5438fe6d747ca9d
Manjunath <hanamanthgad123@gmail.com>
2023-04-27 14:34:40 +0530

    lists

--- programs/list.py
+++ programs/list.py
@@ -43,3 +43,69 @@ motorcycles.append('crv')
 motorcycles = []
 motorcycles.append('aa')
 motorcycles.append('bb')
+
+# Inserting Elements into a List
+
+motorcycles.insert(0, 'zz') # zz,aa,bb
+
+# Removing Elements from a List
+del motorcycles[0] ## aa,bb
+
+# Removing an Item Using the pop() Method
+popped_motorcycle = motorcycles.pop() #bb
+
+# Sorting a list
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+c = cars.sort(reverse=True) #Nonetype
+cars.sort(reverse=True) #'bmw', 'audi', 'toyota', 'subaru'
+
+
+# sorted function
+
+'''
+To maintain the original order of a list but present it in a sorted order, you can use the sorted() function. The sorted() function lets you display your list in a particular order, but doesn’t affect the actual order of the list.
+'''
+sorted(cars)
+
+
+# rreverse order
+cars.reverse()
+cars=cars[::-1] #subaru toyota audi bmw 
+
+places = ['ab','bc','cd','de','ef']
+sorted(places)
+places.reverse()
+places.sort(reverse=True)
+len(places)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
