7e662047ffd353677ba49824841a0615bc78e99c
Manjunath <hanamanthgad123@gmail.com>
2023-06-14 01:30:39 +0530

    strings

--- python-dsa/reverse_words.py
+++ python-dsa/reverse_words.py
@@ -12,3 +12,39 @@ for word in words:
     
     
 print(" ".join(string))
+
+
+# ----------
+
+def reverse(string):
+    words = string.split('.')
+    left = 0
+    right = len(words) - 1
+    
+    while left < right:
+        words[left], words[right] = words[right], words[left]
+        left =+1
+        right -=1
+    return '.'.join(words)
+
+#string = 'i.am.very.ok.with.this.learning'
+
+words = 'i.am.very.ok.with.this.learning'
+reverse = reverse(words)
+print(reverse)
+
+
+'''
+    Time Complexity:
+        Splitting the string into words using the split function has a time complexity of O(n), where n is the length of the input string.
+        Reversing the list of words using [::-1] has a time complexity of O(n).
+        Joining the reversed words back into a string using join has a time complexity of O(n).
+        Overall, the time complexity of the solution is O(n).
+
+    Space Complexity:
+        The solution utilizes additional space to store the list of words and the reversed words, both of which have a space complexity of O(n), where n is the number of words in the sentence.
+        The reversed sentence also requires O(n) space.
+        Therefore, the space complexity of the solution is O(n).
+
+Considering the simplicity of the algorithm and the linear time and space complexity, this solution is considered efficient for reversing a string without reversing its individual words.
+'''
